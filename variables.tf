73cfe45603ffcb0b143c09cf7ccdf3bdd1f43798
Alex Seymour <axeal@users.noreply.github.com>
2025-08-06 16:59:50 +0100

    Add rancher-latest fallback repo

--- variables.tf
+++ variables.tf
@@ -43,7 +43,7 @@ variable "ssh_keys" {
 }
 
 variable "rancher_chart_repo" {
-  default = "https://releases.rancher.com/server-charts/stable"
+  default = ""
 }
 
 variable "rancher_version" {
@@ -53,4 +53,3 @@ variable "rancher_version" {
 variable "admin_password" {
   default = "adminadminadmin"
 }
-
